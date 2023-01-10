64ae450a7653746f6c5c1eccdfac8676ddbcc1bf
Laura <120367965+LCAW@users.noreply.github.com>
2023-01-10 12:32:07 +0100

    Update Bubble.c

--- Bubble.c
+++ Bubble.c
@@ -2,12 +2,12 @@
 // ich wurde geändert
 // Werte einlesen
 int main(){
-int werte[20],i,j,k,temp,e;
+int i,j,k,temp,e;
     /*for(i=0; i<20; i++) {
     printf("\nBitte geben Sie einen Wert ein (ganze Zahl): ");
     scanf("%d", &werte[i]);
     }*/
-    werte[20]={1,4,3,5,64,75,3423,43,54,636,555,435,6,44,90,8878,1,2,3,4};
+   int werte[20]= {4,3,5,64,75,33,43,54,636,555,45,6,44,90,87,1,2,3,4};
     
     for(j=0;j<20;j++){
         for(k=0;k<20-j;k++){
@@ -22,5 +22,3 @@ int werte[20],i,j,k,temp,e;
     for(e=0;e<20;e++){
         printf("    %d\n",werte[e]);
     }
-    return 0;
-}
